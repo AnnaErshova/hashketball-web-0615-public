09929bdd8807d9fab15ed378fc098c1336b0ad3b
AnnaErshova <anna.ershova@gmail.com>
2015-06-09 11:05:17 -0400

    first stab at re-written hashketball

--- hashketball.rb
+++ hashketball.rb
@@ -4,151 +4,157 @@
 # the test for this is written that is basically tests for every player, so the stats actually have to be correct
 
 
-def game_hash #doesn't expect arguments
-  # where do players go? the test is not super explicit; put them as a key within teams, but value will be an array
-  hashketball = { # opens game hash
-                :home => { #opens home team #home_or_away
-                     :team_name => "Brooklyn Nets", 
-                     :colors => ["Black", "White"],
-                     :players => { # open players
-                      "Alan Anderson" => { # opens individual player
-                          :number => 0,
-                          :shoe => 16,
-                          :points => 22,
-                          :rebounds => 12,
-                          :assists => 12,
-                          :steals => 3,
-                          :blocks => 1,
-                          :slam_dunks => 1
-                          },
-                        "Reggie Evans" => {
-                          :number => 30,
-                          :shoe => 14,
-                          :points => 12,
-                          :rebounds => 12,
-                          :assists => 12,
-                          :steals => 12,
-                          :blocks => 12,
-                          :slam_dunks => 7
-                          },                        
-                        "Brook Lopez" => {
-                          :number => 11,
-                          :shoe => 17,
-                          :points => 17,
-                          :rebounds => 19,
-                          :assists => 10,
-                          :steals => 3,
-                          :blocks => 1,
-                          :slam_dunks => 15
-                          },
-                        "Mason Plumlee" => {
-                          :number => 1,
-                          :shoe => 19,
-                          :points => 26,
-                          :rebounds => 12,
-                          :assists => 6,
-                          :steals => 3,
-                          :blocks => 8,
-                          :slam_dunks => 5
-                          },                          
-                        "Jason Terry" => {
-                          :number => 31,
-                          :shoe => 15,
-                          :points => 19,
-                          :rebounds => 2,
-                          :assists => 2,
-                          :steals => 4,
-                          :blocks => 11,
-                          :slam_dunks => 1
-                        } #closes Jason Terry
-                      } # closes players
-                    }, # closes home team
-                 :away => { # opens away team
-                      :team_name => "Charlotte Hornets", 
-                      :colors => ["Turquoise", "Purple"], # should this be an array?
-                      :players => { # opens players
-                        "Jeff Adrien" => { # opens individual player
-                          :number => 4,
-                          :shoe => 18,
-                          :points => 10,
-                          :rebounds => 1,
-                          :assists => 1,
-                          :steals => 2,
-                          :blocks => 7,
-                          :slam_dunks => 2
-                          },
-                        "Bismak Biyombo" => {
-                          :number => 0,
-                          :shoe => 16,
-                          :points => 12,
-                          :rebounds => 4,
-                          :assists => 7,
-                          :steals => 7,
-                          :blocks => 15,
-                          :slam_dunks => 10
-                          },                        
-                        "DeSagna Diop" => {
-                          :number => 2,
-                          :shoe => 14,
-                          :points => 24,
-                          :rebounds => 12,
-                          :assists => 12,
-                          :steals => 4,
-                          :blocks => 5,
-                          :slam_dunks => 5
-                          },
-                        "Ben Gordon" => {
-                          :number => 8,
-                          :shoe => 15,
-                          :points => 33,
-                          :rebounds => 3,
-                          :assists => 2,
-                          :steals => 1,
-                          :blocks => 1,
-                          :slam_dunks => 0
-                          },                          
-                        "Brendan Haywood" => {
-                          :number => 33,
-                          :shoe => 15,
-                          :points => 6,
-                          :rebounds => 12,
-                          :assists => 12,
-                          :steals => 22,
-                          :blocks => 5,
-                          :slam_dunks => 12
-                          } # closes Brendan Haywood
-                        } # closes players                    
-                      } # closes away team
-                    } # closes the hash
-  # try returning hashkerball hash?
+def game_hash
+  {
+    :home => { :team_name => "Charlotte Hornets",
+               :colors => ["Turquoise", "Purple"],
+               :players => [
+                 {:player_name => "Jeff Adrien",
+                  :number => 4,
+                  :shoe => 18,
+                  :points => 10,
+                  :rebounds => 1,
+                  :assists => 1,
+                  :steals => 2,
+                  :blocks => 7,
+                  :slam_dunks => 2
+                 },
+                 {:player_name => "Bismak Biyombo",
+                  :number => 0,
+                  :shoe => 16,
+                  :points => 12,
+                  :rebounds => 4,
+                  :assists => 7,
+                  :steals => 7,
+                  :blocks => 15,
+                  :slam_dunks => 10
+                 },
+                 {:player_name => "DeSagna Diop",
+                  :number => 2,
+                  :shoe => 14,
+                  :points => 24,
+                  :rebounds => 12,
+                  :assists => 12,
+                  :steals => 4,
+                  :blocks => 5,
+                  :slam_dunks => 5
+                 },
+                 {:player_name => "Ben Gordon",
+                  :number => 8,
+                  :shoe => 15,
+                  :points => 33,
+                  :rebounds => 3,
+                  :assists => 2,
+                  :steals => 1,
+                  :blocks => 1,
+                  :slam_dunks => 0
+                 },
+                 {:player_name => "Brendan Haywood",
+                  :number => 33,
+                  :shoe => 15,
+                  :points => 6,
+                  :rebounds => 12,
+                  :assists => 12,
+                  :steals => 22,
+                  :blocks => 5,
+                  :slam_dunks => 12
+                 }
+               ]
+            },
+    :away => { :team_name => "Brooklyn Nets",
+               :colors => ["Black", "White"],
+               :players => [
+                 {:player_name => "Alan Anderson",
+                  :number => 0,
+                  :shoe => 16,
+                  :points => 22,
+                  :rebounds => 12,
+                  :assists => 12,
+                  :steals => 3,
+                  :blocks => 1,
+                  :slam_dunks => 1
+                 },
+                 {:player_name => "Reggie Evans",
+                  :number => 30,
+                  :shoe => 14,
+                  :points => 12,
+                  :rebounds => 12,
+                  :assists => 12,
+                  :steals => 12,
+                  :blocks => 12,
+                  :slam_dunks => 7
+                 },
+                 {:player_name => "Brook Lopez",
+                  :number => 11,
+                  :shoe => 17,
+                  :points => 17,
+                  :rebounds => 19,
+                  :assists => 10,
+                  :steals => 3,
+                  :blocks => 1,
+                  :slam_dunks => 15
+                 },
+                 {:player_name => "Mason Plumlee",
+                  :number => 1,
+                  :shoe => 19,
+                  :points => 26,
+                  :rebounds => 12,
+                  :assists => 6,
+                  :steals => 3,
+                  :blocks => 8,
+                  :slam_dunks => 5
+                 },
+                 {:player_name => "Jason Terry",
+                  :number => 31,
+                  :shoe => 15,
+                  :points => 19,
+                  :rebounds => 2,
+                  :assists => 2,
+                  :steals => 4,
+                  :blocks => 11,
+                  :slam_dunks => 1
+                 }
+               ]
+    }
+  }
 end
 
 # Return the number of points scored for any player, given that player's name  ('name') as a string. 
 # .each_key
+# !!!
 def num_points_scored(name)
-  points_scored = nil
-  game_hash.each do |home_or_away, team_details| 
-    team_details[:players].each do |player, stats|
-      if name == player.to_s
-        points_scored = team_details[:players][name][:points]
-      end
+    # new_hash = {}
+    points_scored = nil
+    game_hash.each do |home_or_away, team_details|
+        team_details[:players].each do |one_hash|  # one_hash is an array
+            one_hash.each do |key1,value1|
+                if one_hash.has_value? (name)
+                    points_scored = one_hash[:points]
+                end
+            end
+        end
     end
-  end
-  points_scored
+    points_scored
 end
+
 
 # returns shoe size of each player given player name ('name')
 # same method as above for points
+# !!!
+
 def shoe_size(name)
-  shoe_size = nil
-  game_hash.each do |home_or_away, team_details|
-    team_details[:players].each do |player, stats|
-      if name == player.to_s
-        shoe_size = team_details[:players][name][:shoe]
-      end
-    end # end the do block
-  end
-  shoe_size
+    # new_hash = {}
+    shoe_size = nil
+    game_hash.each do |home_or_away, team_details|
+        team_details[:players].each do |one_hash|  # one_hash is an array
+            one_hash.each do |key1,value1|
+                if one_hash.has_value? (name)
+                    shoe_size = one_hash[:shoe]
+                end
+            end
+        end
+    end
+    shoe_size
 end
 
 # Return both colors for any team, given the team name. Call the method team_colors.
